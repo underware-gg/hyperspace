48fddeee17fdb4eda9f146b5394813b5de05e94a
James Addison <siliconjungle.io@gmail.com>
2022-11-27 14:53:36 +1100

    update config to use new server

--- core/networking/config.js
+++ core/networking/config.js
@@ -1,4 +1,2 @@
-// export const serverUrl = 'ws://localhost:8080'
 // export const serverUrl = 'ws://localhost:8787'
-export const serverUrl = 'wss://crdt-server.offlinefirst.workers.dev'
-// export const serverUrl = 'wss://metaverse-in-a-box.herokuapp.com/'
+export const serverUrl = 'wss://crdt-server.fundaomental.workers.dev'
