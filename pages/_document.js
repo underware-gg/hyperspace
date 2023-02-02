60ce836803830f84f733adb7d185f144b00c95e1
R13 <rbentos@yahoo.com>
2023-02-02 13:04:00 -0300

    Embedded used google fonts

--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,6 @@ import { Html, Head, Main, NextScript } from 'next/document'
 const Document = () => (
   <Html lang='en'>
     <Head />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" />
-    <link href="https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=block" rel="stylesheet" />
     <body>
       <Main />
       <NextScript />
