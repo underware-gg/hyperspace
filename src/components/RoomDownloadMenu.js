9bc33797bf1b1f05461ad98bc7d714f16f081209
R13 <rbentos@yahoo.com>
2023-02-10 18:24:35 -0300

    Very basic Endless Crawler import

--- src/components/RoomDownloadMenu.js
+++ src/components/RoomDownloadMenu.js
@@ -6,6 +6,9 @@ import usePermission from '@/hooks/usePermission'
 import Button from '@/components/Button'
 import FileSelectButton from '@/components/FileSelectButton'
 import * as ClientRoom from '@/core/networking'
+import * as CrawlerData from '@rsodre/crawler-data'
+import * as Map from '@/core/components/map'
+const BN = require('bn.js');
 
 const _downloadRoomData = (slug) => {
   const room = ClientRoom.get()
@@ -41,6 +44,27 @@ const RoomDownloadMenu = () => {
     reader.readAsText(fileObject)
   }
 
+  const _importCrawlerChamber = () => {
+    const slug = window.prompt('DID to invite', 'S1W1')
+    if (!slug) return
+    for (let tokenId = 1; tokenId <= CrawlerData.getChamberCount(); ++tokenId) {
+      const coords = CrawlerData.getTokenIdToCoords(tokenId)
+      if (coords.slug == slug) {
+        const chamberData = CrawlerData.getChamberData(coords.coord)
+        // console.log(chamberData)
+        const bitmap = new BN(chamberData.bitmap.slice(2), 16)
+        for (let x = 0; x < 16; ++x) {
+          for (let y = 0; y < 15; ++y) {
+            const i = y * 16 + x
+            const bit = bitmap.and(new BN('1').shln(255-i)).eq(new BN('0')) ? 0 : 1
+            Map.update('world', x, y, bit ? 8 : 5)
+          }
+        }
+        return
+      }
+    }
+  }
+
   return (
     <HStack>
       <a id='download-room-data' href='#' hidden></a>
@@ -59,6 +83,14 @@ const RoomDownloadMenu = () => {
         accept='.json'
         onSelect={(fileObject) => _uploadRoomData(fileObject)}
       />
+      <Button
+        disabled={!canEdit}
+        variant='outline'
+        size='sm'
+        onClick={() => _importCrawlerChamber()}
+      >
+        Import Crawler Chamber
+      </Button>
     </HStack>
   )
 }
