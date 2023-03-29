31a0564155e29cf7979e8764bbe531d453af04e3
R13 <rbentos@yahoo.com>
2023-03-28 22:25:26 -0300

    Deleted deprecated singletons

--- src/core/networking/index.js
+++ src/core/networking/index.js
@@ -5,26 +5,3 @@ import { serverUrl } from './config'
 export const create = (slug, store, agent = null) => {
   return new ClientRoom(serverUrl, slug, store, agent)
 }
-
-
-//-------------------------------
-// DEPRECATION WARNING
-//
-// The singleton ClientRoom will be removed soon
-// avoid using it!
-//
-import { getRemoteStore } from '@/core/singleton'
-
-// Create the main Room singleton
-
-let clientRoom = null
-
-export const init = (slug) => {
-  if (clientRoom !== null) {
-    console.warn(`ClientRoom.init() can be called only once. Use ClientRoom.create() to open another document`)
-    return
-  }
-  clientRoom = create(slug, getRemoteStore())
-}
-
-export const get = () => clientRoom
