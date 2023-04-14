0ecdc084ec28b4667bc7537ba79d5a31e168064a
R13 <rbentos@yahoo.com>
2023-04-13 23:20:20 -0300

    Configured wagmi, connectkit, /pages/stateToken
    yarn add connectkit wagmi ethers@^5

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,39 @@
 import { theme } from '@/styles/theme'
+import { WagmiConfig, createClient } from 'wagmi'
+import { mainnet, goerli } from 'wagmi/chains'
+import { ConnectKitProvider, getDefaultClient } from 'connectkit'
 import { ChakraProvider } from '@chakra-ui/react'
 import { RoomProvider } from '@/hooks/RoomContext'
 import { VeridaProvider } from '@/hooks/VeridaContext'
 import Head from '@/components/Head'
 import '/styles/styles.scss'
 
+const client = createClient(
+  getDefaultClient({
+    appName: 'Hyperbox',
+    chains: [goerli],
+    autoConnect: false,
+    // alchemyId: process.env.ALCHEMY_ID,
+  }),
+)
+
+const connectKitTheme = {
+  '--ck-font-family': 'Share Tech Mono',
+}
+
 const MyApp = ({ Component, pageProps }) => (
-  <ChakraProvider theme={theme}>
-    <VeridaProvider>
-      <RoomProvider>
-        <Head />
-        <Component {...pageProps} />
-      </RoomProvider>
-    </VeridaProvider>
-  </ChakraProvider>
+  <WagmiConfig client={client}>
+    <ConnectKitProvider theme='midnight' customTheme={connectKitTheme}>
+      <ChakraProvider theme={theme}>
+        <VeridaProvider>
+          <RoomProvider>
+            <Head />
+            <Component {...pageProps} />
+          </RoomProvider>
+        </VeridaProvider>
+      </ChakraProvider>
+    </ConnectKitProvider>
+  </WagmiConfig>
 )
 
 export default MyApp
--- pages/pages/stateToken.js
+++ pages/pages/stateToken.js
@@ -1,15 +1,16 @@
+import { useState } from 'react'
 import Link from 'next/link'
+import { ConnectKitButton } from 'connectkit'
 import {
   VStack,
 } from '@chakra-ui/react'
 import Layout from '@/components/Layout'
 
-const PagesPage = () => {
+const PagesPage = ({ slug }) => {
   return (
     <Layout height='100vh'>
       <VStack align='stretch' w='100%' alignItems='center'>
-        <Link href='/pages/dual'>▫️ Dual Room Demo ▫️</Link>
-        <Link href='/pages/stateToken'>▫️ Hyperbox State Token ▫️</Link>
+        <ConnectKitButton />
       </VStack>
     </Layout>
   )
