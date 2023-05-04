59fbf2e5926fbdbf39dbc5c94d4e9aec0f168030
R13 <rbentos@yahoo.com>
2023-05-04 19:53:31 -0300

    Snapshot modal: delete item button (development mode only)

--- src/components/Snapshot.js
+++ src/components/Snapshot.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect, useMemo, useRef, useCallback } from 'react'
 import {
   Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon,
-  HStack,
+  HStack, VStack, Spacer,
   Text,
   Box,
   Checkbox,
-  VStack,
 } from '@chakra-ui/react'
-import { DownloadIcon } from '@chakra-ui/icons'
+import {
+  DownloadIcon,
+  DeleteIcon,
+} from '@chakra-ui/icons'
 import { useDocumentIds } from '@/hooks/useDocumentIds'
 import { useDocumentTypes } from '@/hooks/useDocumentTypes'
 import { MapPreview } from '@/components/Map2D'
@@ -73,13 +75,6 @@ const Snapshot = ({
 }
 
 let _logged = false
-const _log = (type, id, content) => {
-  console.log(`${type} [${id}]:`, content)
-  if (!_logged) {
-    alert(`Document dumped to the console`)
-    _logged = true
-  }
-}
 
 const Type = ({
   store,
@@ -89,15 +84,29 @@ const Type = ({
 }) => {
   const ids = useDocumentIds(type, store) ?? []
 
+  const _log = (type, id, content) => {
+    console.log(`${type} [${id}]:`, content)
+    if (!_logged) {
+      alert(`Document dumped to the console`)
+      _logged = true
+    }
+  }
+
+  const _delete = (type, id) => {
+    store.setDocument(type, id, null)
+    console.log(`deleted ${type} [${id}]`)
+  }
+
   const items = useMemo(() => {
     let result = []
-    for (const id of ids) {
+    for (let i = 0; i < ids.length; ++i) {
+      const id = ids[i]
       const doc = store?.getDocument(type, id) ?? null
       if (!doc) continue
       const name = doc.name ?? doc.slug ?? doc?.id ?? null
       const content = doc.content ? doc.content.slice(0, 20) : null
       result.push(
-        <VStack key={id} alignItems='left'>
+        <VStack key={id} alignItems='left' className={`SlugItem${i%2}`}>
           <HStack>
             <DownloadIcon boxSize='0.8em' className='Clickable' onClick={() => _log(type, id, doc)} />
             <Text>
@@ -105,6 +114,10 @@ const Type = ({
               {name && ` (${name})`}
               {content && ` : ${content}...`}
             </Text>
+            <Spacer />
+            {process.env.DEV &&
+              <DeleteIcon boxSize='0.8em' className='Clickable' onClick={() => _delete(type, id)} />
+            }
           </HStack>
           {type == 'map' && <MapPreview store={store} />}
         </VStack>
@@ -133,7 +146,6 @@ const Type = ({
         <AccordionIcon />
       </AccordionButton>
       <AccordionPanel style={{
-        backgroundColor: '#222c',
         padding: '0.25em 0.5em',
       }}>
         {items}
